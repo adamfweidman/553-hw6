f67ba1e89942bd538fd78272e293285e9a40b821
Micah Weitzman <wmicah@seas.upenn.edu>
2021-05-02 19:20:17 +0000

    bug fixes

--- client.py
+++ client.py
@@ -38,7 +38,7 @@ def recv_thread_func(wrap, cond_filled, sock):
     file = open("log.mp3", "wb")
     while True:
         # TODO
-        recv_data = sock.recv(1024) # test value 
+        recv_data = sock.recv(4097) # test value 
         command = struct.unpack("1s", recv_data[0])
         print(command)
         if command[0] == "l": 
@@ -55,6 +55,7 @@ def recv_thread_func(wrap, cond_filled, sock):
             file.write(data)
         else: 
             continue
+        sleep(1)
         
 
 
