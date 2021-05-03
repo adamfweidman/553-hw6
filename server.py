680a79905a7d200727d65970f34652a5f4e52dee
Micah Weitzman <wmicah@seas.upenn.edu>
2021-05-03 18:58:14 +0000

    bug fix

--- server.py
+++ server.py
@@ -49,7 +49,7 @@ def client_write(client):
         if command == "list":
             data = struct.pack('1s',b'l')
             data += pickle.dumps((list(songNameToData.keys())), protocol=2)
-            data += b"0"(2049 - len(data))
+            data += b"0" * (2049 - len(data))
             client.s.sendall(data)
             # print(data)
         elif command == "play":
